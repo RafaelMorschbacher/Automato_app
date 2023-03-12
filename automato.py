0bc708d3d2d9c12b171ebf3d552a5dc546a49815
Mateus <mateuskienzle@gmail.com>
2023-03-11 21:49:15 -0300

    Add outros estados, de acordo com estrutura que foi feita no jflap

--- automato.py
+++ automato.py
@@ -54,25 +54,115 @@ def askName():
     print("Olá " + name + "!")
 
 q0 = State(
-    'q0',
+    'Menu inicial',
     askName,
-    [Transition('a', 'q1'), Transition('b', 'q0')],
+    [Transition('q', 'q1'), Transition('b', 'q2')],
     False
 )
-
 q1 = State(
-    'q1',
+    'Quit game',
     myAction,
-    [Transition('a', 'q0'), Transition('b', 'q1')],
+    [],
+    True
+)
+q2 = State(
+    'Hall de entrada',
+    myAction,
+    [Transition('e', 'q3'), Transition('c', 'q11')],
+    False
+)
+q3 = State(
+    'Andar de cima',
+    myAction,
+    [Transition('w', 'q4'), Transition('c', 'q7'), Transition('r', 'q2')],
+    False
+)
+q4 = State(
+    'Quarto',
+    myAction,
+    [Transition('g', 'q5'), Transition('t', 'q6'), Transition('r', 'q3')],
+    False
+)
+q5 = State(
+    'Garrafa de veneno',
+    myAction,
+    [],
+    False
+)
+q6 = State(
+    'Televisão: câmera de segurança',
+    myAction,
+    [Transition('t', 'q6'), Transition('r', 'q4')],
+    False
+)
+q7 = State(
+    'Corredor do andar de cima',
+    myAction,
+    [Transition('n', 'q8'), Transition('p', 'q9')],
+    False
+)
+q8 = State(
+    'Papel com recado',
+    myAction,
+    [Transition('r', 'q7')],
+    False
+)
+q9 = State(
+    'Porta',
+    myAction,
+    [Transition('h', 'q10'), Transition('s', 'q17')],
+    False
+)
+q10 = State(
+    'Pulou da janela e morreu',
+    myAction,
+    [],
+    False
+)
+q11 = State(
+    'Corredor do andar térreo',
+    myAction,
+    [Transition('a', 'q12'),  Transition('p', 'q15'), Transition('r', 'q2')],
+    False
+)
+q12 = State(
+    'Alçapão',
+    myAction,
+    [Transition('c', 'q13'), Transition('r', 'q12')],
+    False
+)
+q13 = State(
+    'Corredor do alçapão',
+    myAction,
+    [Transition('p', 'q14'), Transition('r', 'q12')],
+    False
+)
+q14 = State(
+    'Preso no alçapão',
+    myAction,
+    [Transition('r', 'q14')],
+    False
+)
+q15 = State(
+    'Viu a luz da janela',
+    myAction,
+    [Transition('l', 'q16'), Transition('r', 'q15')],
+    False
+)
+q16 = State(
+    'Foge para o jardim pela janela',
+    myAction,
+    [Transition('j', 'q17')],
+    False
+)
+q17 = State(
+    'Jardim de saída',
+    myAction,
+    [],
     True
 )
 
-q17 = [
-    'q17',
-    myAction(),
-    [Transition('b','q0'), Transition('q','q9')],
-    False
-]
+
 
 myAut = Automato([q0,q1], ['a','b'], 'q0')
 
