1a777a2bec5890884c885bd07f69c24a4091ae55
Mateus <mateuskienzle@gmail.com>
2023-03-11 23:30:52 -0300

    Ajustei um bug que tava tendo em relacao ao formato dos ids que eu tinha colocado dentro dos estados

--- automato.py
+++ automato.py
@@ -1,23 +1,18 @@
 # Definições
-
 class State:
     def __init__(self, id, action, transitions ,isFinal):
         self.id = id
         self.action = action
         self.transitions = transitions
         self.isFinal = isFinal
-
     def getTransition(self, symbol):
         for transition in self.transitions:
             if transition.symbol == symbol:
                 return transition
-
 class Transition:
     def __init__(self, symbol, dst):
         self.symbol = symbol
         self.dst = dst
-
-
 class Automato:
     def __init__(self, states, alph, current_state_id):
         self.states = states
@@ -29,10 +24,12 @@ class Automato:
                 if state.id == id:
                     return state
             return False
-
     def readSymbol(self, symbol):
         transition = self.current_state.getTransition(symbol)
         self.current_state = self.getState(transition.dst)
+        # print(self.current_state)
+        # import pdb
+        # pdb.set_trace()
         self.current_state.action()
     def readWord(self, word):
         self.current_state.action()
@@ -42,128 +39,122 @@ class Automato:
             print("ACEITA")
         else:
             print("REJEITA")
-
-
-
 # Testes:
 def myAction():
     print('Action!')
-
 def askName():
     name = input('Digite seu nome: ')
     print("Olá " + name + "!")
-
 q0 = State(
-    'Menu inicial',
+    'q0', #'Menu inicial',
     askName,
     [Transition('q', 'q1'), Transition('b', 'q2')],
-    False
+    True
 )
 q1 = State(
-    'Quit game',
+    'q1', #'Quit game',
     myAction,
     [],
     True
 )
 q2 = State(
-    'Hall de entrada',
+    'q2', # 'Hall de entrada',
     myAction,
     [Transition('e', 'q3'), Transition('c', 'q11')],
     False
 )
 q3 = State(
-    'Andar de cima',
+    'q3', #'Andar de cima',
     myAction,
     [Transition('w', 'q4'), Transition('c', 'q7'), Transition('r', 'q2')],
     False
 )
 q4 = State(
-    'Quarto',
+    'q4', # Quarto',
     myAction,
     [Transition('g', 'q5'), Transition('t', 'q6'), Transition('r', 'q3')],
     False
 )
 q5 = State(
-    'Garrafa de veneno',
+    'q5', # 'Garrafa de veneno',
     myAction,
     [],
     False
 )
 q6 = State(
-    'Televisão: câmera de segurança',
+    'q6', # 'Televisão: câmera de segurança',
     myAction,
     [Transition('t', 'q6'), Transition('r', 'q4')],
     False
 )
 q7 = State(
-    'Corredor do andar de cima',
+    'q7', # 'Corredor do andar de cima',
     myAction,
     [Transition('n', 'q8'), Transition('p', 'q9')],
     False
 )
 q8 = State(
-    'Papel com recado',
+    'q8', # 'Papel com recado',
     myAction,
     [Transition('r', 'q7')],
     False
 )
 q9 = State(
-    'Porta',
+    'q9', # 'Porta',
     myAction,
     [Transition('h', 'q10'), Transition('s', 'q17')],
     False
 )
 q10 = State(
-    'Pulou da janela e morreu',
+    'q10', # 'Pulou da janela e morreu',
     myAction,
     [],
     False
 )
 q11 = State(
-    'Corredor do andar térreo',
+    'q11', # 'q11',
     myAction,
     [Transition('a', 'q12'),  Transition('p', 'q15'), Transition('r', 'q2')],
     False
 )
 q12 = State(
-    'Alçapão',
+    'q12', # 'Alçapão',
     myAction,
     [Transition('c', 'q13'), Transition('r', 'q12')],
     False
 )
 q13 = State(
-    'Corredor do alçapão',
+    'q13', # 'Corredor do alçapão',
     myAction,
     [Transition('p', 'q14'), Transition('r', 'q12')],
     False
 )
 q14 = State(
-    'Preso no alçapão',
+    'q14', # 'Preso no alçapão',
     myAction,
     [Transition('r', 'q14')],
     False
 )
 q15 = State(
-    'Viu a luz da janela',
+    'q15', # 'Viu a luz da janela',
     myAction,
     [Transition('l', 'q16'), Transition('r', 'q15')],
     False
 )
 q16 = State(
-    'Foge para o jardim pela janela',
+    'q16', # 'Foge para o jardim pela janela',
     myAction,
     [Transition('j', 'q17')],
     False
 )
 q17 = State(
-    'Jardim de saída',
+    'q17', # 'Jardim de saída',
     myAction,
     [],
     True
 )
 
 
+myAut = Automato([q0,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17], ['q','b','e', 'r', 'c', 'w', 'g', 't', 'p', 'n', 'h', 's', 'j', 'l', 'a'], 'q0')
 
-myAut = Automato([q0,q1], ['a','b'], 'q0')
-
-myAut.readWord('abbaaa')
+myAut.readWord('becps')
