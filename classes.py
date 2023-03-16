6658c2a9e32bf2de7922467472d6edd2b5aef417
RafaelMorschbacher <rafa2002cunha@gmail.com>
2023-03-16 14:55:27 -0300

    Divisão em arquivos separados
    Divisão em arquivos de CLASSES, ESTADOS, AÇÕES, e MAIN (AUTOMATO)

---
+++ classes.py
@@ -0,0 +1,66 @@
+
+def myAction():
+    print("Action")
+
+
+
+class State:
+    def __init__(self, id, action, transitions, isFinal):
+        self.id = id
+        self.action = action
+        self.transitions = transitions
+        self.isFinal = isFinal
+
+    def getTransition(self, symbol):
+        for transition in self.transitions:
+            if transition.symbol == symbol:
+                return transition
+        return False
+
+
+class Transition:
+    def __init__(self, symbol, dst):
+        self.symbol = symbol
+        self.dst = dst
+
+
+class Automato:
+    def __init__(self, states, alph, current_state_id):
+        self.states = states
+        self.alph = alph
+        self.current_state = self.getState(current_state_id)
+
+        self.trap_state = State('qt', myAction, [], False)
+        self.states.append(self.trap_state)
+
+        for state in self.states:
+            for symbol in alph:
+                if not state.getTransition(symbol):
+                    undef_transition = Transition(symbol, 'qt')
+                    state.transitions.append(undef_transition)
+
+
+    def getState(self, id):
+        for state in self.states:
+            if state.id == id:
+                return state
+        return False
+
+    def readSymbol(self, symbol):
+        transition = self.current_state.getTransition(symbol)
+        self.current_state = self.getState(transition.dst)
+        # print(self.current_state)
+        # import pdb
+        # pdb.set_trace()
+        self.current_state.action()
+
+    def readWord(self, word):
+        self.current_state = self.getState('q0')
+        self.current_state.action()
+        for character in word:
+            self.readSymbol(character)
+        if self.current_state.isFinal:
+            print("ACEITA")
+        else:
+            print("REJEITA")
+
---
+++ states.py
@@ -0,0 +1,111 @@
+from classes import State, Transition
+import actions
+
+q0 = State(
+    'q0',  # 'Menu inicial',
+    actions.menu,
+    [Transition('q', 'q1'), Transition('b', 'q2')],
+    True
+)
+q1 = State(
+    'q1',  # 'Quit game',
+    actions.quit_game,
+    [],
+    True
+)
+q2 = State(
+    'q2',  # 'Hall de entrada',
+    actions.entry_hall,
+    [Transition('e', 'q3'), Transition('c', 'q11')],
+    False
+)
+q3 = State(
+    'q3',  # 'Andar de cima',
+    actions.second_floor,
+    [Transition('w', 'q4'), Transition('c', 'q7'), Transition('r', 'q2')],
+    False
+)
+q4 = State(
+    'q4',  # Quarto',
+    actions.bedroom,
+    [Transition('g', 'q5'), Transition('t', 'q6'), Transition('r', 'q3')],
+    False
+)
+q5 = State(
+    'q5',  # 'Garrafa de veneno',
+    actions.bottle,
+    [],
+    False
+)
+q6 = State(
+    'q6',  # 'Televisão: câmera de segurança',
+    actions.television,
+    [Transition('t', 'q6'), Transition('r', 'q4')],
+    False
+)
+q7 = State(
+    'q7',  # 'Corredor do andar de cima',
+    actions.second_floor_hall,
+    [Transition('n', 'q8'), Transition('p', 'q9')],
+    False
+)
+q8 = State(
+    'q8',  # 'Papel com recado',
+    actions.paper_note,
+    [Transition('r', 'q7')],
+    False
+)
+q9 = State(
+    'q9',  # 'Porta',
+    actions.door_second_floor,
+    [Transition('h', 'q10'), Transition('s', 'q17')],
+    False
+)
+q10 = State(
+    'q10',  # 'Pulou da janela e morreu',
+    actions.window_jump,
+    [],
+    False
+)
+q11 = State(
+    'q11',  # 'q11',
+    actions.first_floor_hall,
+    [Transition('a', 'q12'), Transition('p', 'q15'), Transition('r', 'q2')],
+    False
+)
+q12 = State(
+    'q12',  # 'Alçapão',
+    actions.trapdoor,
+    [Transition('c', 'q13'), Transition('r', 'q12')],
+    False
+)
+q13 = State(
+    'q13',  # 'Corredor do alçapão',
+    actions.loft_hall,
+    [Transition('p', 'q14'), Transition('r', 'q12')],
+    False
+)
+q14 = State(
+    'q14',  # 'Preso no alçapão',
+    actions.door_prision,
+    [Transition('r', 'q14')],
+    False
+)
+q15 = State(
+    'q15',  # 'Viu a luz da janela',
+    actions.door_first_floor,
+    [Transition('l', 'q16'), Transition('r', 'q15')],
+    False
+)
+q16 = State(
+    'q16',  # 'Foge para o jardim pela janela',
+    actions.window_first_floor,
+    [Transition('j', 'q17')],
+    False
+)
+q17 = State(
+    'q17',  # 'Jardim de saída',
+    actions.garden_of_freedom,
+    [],
+    True
+)
